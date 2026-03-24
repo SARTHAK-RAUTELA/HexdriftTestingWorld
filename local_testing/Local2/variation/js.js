2455d8e2f493d46f2c32e63fd0aa41a5d4760d37
Hexdrift <150795432+SARTHAK-RAUTELA@users.noreply.github.com>
2026-03-24 18:51:50 +0530

    control goal js

--- local_testing/Local2/variation/js.js
+++ local_testing/Local2/variation/js.js
@@ -1,8 +1,7 @@
 (function () {
     try {
         var debug = 1;
-        var variation_name = "cre-t-2-Availability_Badge_Control";
-
+        var variation_name = "Availability_Badge_control";
         /* ---------------- HELPER ---------------- */
         var _$;
         (function (factory) {
@@ -16,13 +15,11 @@
                     this.value = [s];
                 }
             };
-
             bm.prototype = {
                 each: function (fn) {
                     [].forEach.call(this.value, fn);
                     return this;
                 },
-
                 waitForElement: function (selector, trigger, delayInterval, delayTimeout) {
                     var interval = setInterval(function () {
                         if (_$(selector).value.length) {
@@ -30,23 +27,19 @@
                             trigger();
                         }
                     }, delayInterval);
-
                     setTimeout(function () {
                         clearInterval(interval);
                     }, delayTimeout);
                 },
-
                 live: function (selector, event, callback, context) {
                     function addEvent(el, type, handler) {
                         if (el.attachEvent) el.attachEvent("on" + type, handler);
                         else el.addEventListener(type, handler);
                     }
-
                     Element.prototype.matches =
                         Element.prototype.matches ||
                         Element.prototype.webkitMatchesSelector ||
                         Element.prototype.msMatchesSelector;
-
                     addEvent(context || document, event, function (e) {
                         var el = e.target;
                         while (el && el !== document) {
@@ -59,82 +52,76 @@
                     });
                 }
             };
-
             return function (selector) {
                 return new bm(selector);
             };
         });
-
         var helper = _$();
 
-        /* ---------------- GA FUNCTION ---------------- */
-        function trackGAEvent(eventCategory, eventAction, eventLabel) {
-            if ("ga" in window && "getAll" in window.ga) {
-                ga.getAll()[0].send("event", {
-                    eventCategory: eventCategory,
-                    eventAction: eventAction,
-                    eventLabel: eventLabel
-                });
+        /* ---------------- TIME LOGIC ---------------- */
+        function shouldShowBadge() {
+            var now = new Date();
+            var hour = now.getHours();
+            var day = now.getDay();
+            if (day >= 1 && day <= 5) {
+                return !(hour >= 8 && hour < 17);
             }
+            if (day === 6) {
+                return !(hour >= 8 && hour < 12);
+            }
+            return true;
         }
-
-
-
         function isBadgeEligibleTime() {
             var now = new Date();
             var hour = now.getHours();
             var day = now.getDay();
-
             if (day >= 1 && day <= 5) {
                 return hour >= 8 && hour < 17;
             }
-
             if (day === 6) {
                 return hour >= 8 && hour < 12;
             }
-
             return false;
         }
-
         /* ---------------- INIT ---------------- */
         function init() {
             document.body.classList.add(variation_name);
 
-
-
-            // / Button 1: Telehealth
+            /* ---------------- CONVERT GOALS TRACKING ---------------- */
+            helper.live(".availability-badge", "click", function () {
+                window._conv_q = window._conv_q || [];
+                _conv_q.push(["triggerConversion", "100037484"]);
+            });
+            // Book Telehealth
             helper.live('#hero .elementor-widget-button [href="https://app.13sick.com.au/request-consult?isTelehealth=true"]', "click", function () {
                 window._conv_q = window._conv_q || [];
                 if (isBadgeEligibleTime()) {
+                    // Eligible Time
                     _conv_q.push(["triggerConversion", "100037480"]);
-                    console.log("Convert Goal Fired: Telehealth Eligible Time");
                 } else {
+                    // NOT Eligible Time
                     _conv_q.push(["triggerConversion", "100037481"]);
-                    console.log("Convert Goal Fired: Telehealth NOT Eligible Time");
                 }
             });
-
-            // Button 2: Home Visit
+            // Request Home Visit
             helper.live('#hero .elementor-widget-button [href="https://app.13sick.com.au/request-consult/?isHomeVisit=true"]', "click", function () {
                 window._conv_q = window._conv_q || [];
                 if (isBadgeEligibleTime()) {
+                    // Eligible Time
                     _conv_q.push(["triggerConversion", "100037482"]);
-                    console.log("Convert Goal Fired: Home Visit Eligible Time");
                 } else {
+                    // NOT Eligible Time
                     _conv_q.push(["triggerConversion", "100037483"]);
-                    console.log("Convert Goal Fired: Home Visit NOT Eligible Time");
                 }
             });
         }
-
         /* ---------------- RUN ---------------- */
         helper.waitForElement(
-            "#hero > .e-con-inner .e-child > div:nth-child(2) p",
+            "body",
             init,
             50,
             15000
         );
-
     } catch (e) {
         console.log(e, "error in Test cre-t-2-Availability_Badge");
     }

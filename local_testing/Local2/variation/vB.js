b0228733974a8b1e4251e807cb150b6f04d3442b
Hexdrift <150795432+SARTHAK-RAUTELA@users.noreply.github.com>
2026-03-26 20:20:46 +0530

    client feedback updated

--- local_testing/Local2/variation/vB.js
+++ local_testing/Local2/variation/vB.js
@@ -1,22 +1,73 @@
 (function () {
   try {
-    /* main variables */
-    var debug = 0;
-    var variation_name = "cre-t-1-updatebanner";
+    var debug = 1;
+    var variation_name = "cre-t-3";
 
- 
+    /* ---------------- HELPER ---------------- */
+    var _$;
+    (function (factory) {
+      _$ = factory();
+    })(function () {
+      var bm = function (s) {
+        if (typeof s === "string") {
+          this.value = Array.prototype.slice.call(document.querySelectorAll(s));
+        }
+        if (typeof s === "object") {
+          this.value = [s];
+        }
+      };
 
+      bm.prototype = {
+        each: function (fn) {
+          [].forEach.call(this.value, fn);
+          return this;
+        },
+
+        waitForElement: function (selector, trigger, delayInterval, delayTimeout) {
+          var interval = setInterval(function () {
+            if (_$(selector).value.length) {
+              clearInterval(interval);
+              trigger();
+            }
+          }, delayInterval);
+
+          setTimeout(function () {
+            clearInterval(interval);
+          }, delayTimeout);
+        },
+      };
+
+      return function (selector) {
+        return new bm(selector);
+      };
+    });
+
+    var helper = _$();
+
+    /* ---------------- INIT ---------------- */
     function init() {
-      if (document.querySelector(`.${variation_name}`)) return;
       document.body.classList.add(variation_name);
 
-      /* Initialise variation */
-      waitForElement("body", init, 50, 15000);
+      document.querySelectorAll('#hero > .e-con-inner [data-id="66f5f73"] .elementor-button-text')
+        .forEach(function (el) {
+          el.innerHTML = el.innerHTML.replace('Book Telehealth', 'Speak to a doctor now');
+        });
+
+      document.querySelectorAll('#hero > .e-con-inner [data-id="51ac959"] .elementor-button-text')
+        .forEach(function (el) {
+          el.innerHTML = el.innerHTML.replace('Request Home Visit', 'Request Home Visit now');
+        });
     }
 
-    /* Initialise variation */
-    init();
+    /* ---------------- RUN ---------------- */
+    helper.waitForElement(
+      ".elementor-button-text",
+      init,
+      50,
+      15000
+    );
+
   } catch (e) {
-    if (debug) console.log(e, "error in Test " + variation_name);
+    console.log(e, "error in Test cre-t-3");
   }
 })();

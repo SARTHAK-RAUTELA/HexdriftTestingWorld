9ede4caa632cdb93e36ce7b662954247698ade05
Hexdrift <150795432+SARTHAK-RAUTELA@users.noreply.github.com>
2026-04-07 13:41:35 +0530

    js css

--- local_testing/Local2/variation/js.js
+++ local_testing/Local2/variation/js.js
@@ -1,174 +1,146 @@
+
+// The code below has been added to check logged-in and logged-out users
+try {
+  // Function to wait for an element before executing a trigger function
+  function waitForElement(selector, trigger) {
+    var interval = setInterval(function () {
+      if (
+        document &&
+        document.querySelector(selector) &&
+        document.querySelectorAll(selector).length > 0
+      ) {
+        clearInterval(interval);
+        trigger();
+      }
+    }, 50);
+    setTimeout(function () {
+      clearInterval(interval);
+    }, 15000);
+  }
+
+  // Function to get a cookie value by name
+  function getCookie(cname) {
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let cookies = decodedCookie.split(';');
+    for (let i = 0; i < cookies.length; i++) {
+      let c = cookies[i].trim();
+      if (c.indexOf(name) === 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return null;
+  }
+
+  // Function to check and update body class
+  function updateBodyClass() {
+    let body = document.querySelector("body");
+    if (body) {
+      // Condition to check for logged-in users based on cookies
+      // Checking ll_remember_me cookie
+      if (getCookie("ll_remember_me")) {
+        body.classList.add("logged_in_users");
+      } else {
+        body.classList.remove("logged_in_users");
+      }
+
+      // Checking remember_me cookie
+      if (getCookie("remember_me")) {
+        body.classList.add("logged_in_users");
+      } else {
+        body.classList.remove("logged_in_users");
+      }
+
+    }
+
+
+  }
+
+  // Wait for <body> and then execute updateBodyClass
+  waitForElement("body", updateBodyClass);
+
+} catch (e) {
+  console.error("Error in script: checking user login status", e);
+}
+
+/*
+    Here adding a cookie for the user who visits the pages except the service pages. 
+*/
+
 (function () {
   try {
-    var debug = 1;
-    var variation_name = "cre-t-2-Availability_Badge";
+    var debug = 0;
+    var variation_name = "TT-Global-activationCode";
 
-    /* ---------------- HELPER ---------------- */
-    var _$;
-    (function (factory) {
-      _$ = factory();
-    })(function () {
-      var bm = function (s) {
-        if (typeof s === "string") {
-          this.value = Array.prototype.slice.call(document.querySelectorAll(s));
+    function waitForElement(selector, trigger) {
+      var interval = setInterval(function () {
+        if (
+          document &&
+          document.querySelector(selector) &&
+          document.querySelectorAll(selector).length > 0
+        ) {
+          clearInterval(interval);
+          trigger();
         }
-        if (typeof s === "object") {
-          this.value = [s];
-        }
-      };
-
-      bm.prototype = {
-        each: function (fn) {
-          [].forEach.call(this.value, fn);
-          return this;
-        },
-
-        waitForElement: function (selector, trigger, delayInterval, delayTimeout) {
-          var interval = setInterval(function () {
-            if (_$(selector).value.length) {
-              clearInterval(interval);
-              trigger();
-            }
-          }, delayInterval);
-
-          setTimeout(function () {
-            clearInterval(interval);
-          }, delayTimeout);
-        },
-
-        live: function (selector, event, callback, context) {
-          function addEvent(el, type, handler) {
-            if (el.attachEvent) el.attachEvent("on" + type, handler);
-            else el.addEventListener(type, handler);
-          }
-
-          Element.prototype.matches =
-            Element.prototype.matches ||
-            Element.prototype.webkitMatchesSelector ||
-            Element.prototype.msMatchesSelector;
-
-          addEvent(context || document, event, function (e) {
-            var el = e.target;
-            while (el && el !== document) {
-              if (el.matches(selector)) {
-                callback.call(el, e);
-                break;
-              }
-              el = el.parentElement;
-            }
-          });
-        }
-      };
-
-      return function (selector) {
-        return new bm(selector);
-      };
-    });
-
-    var helper = _$();
-
-   
-
-    /* ---------------- TIME LOGIC ---------------- */
-    function shouldShowBadge() {
-      var now = new Date();
-      var hour = now.getHours();
-      var day = now.getDay();
-
-      if (day >= 1 && day <= 5) {
-        return !(hour >= 8 && hour < 17);
-      }
-
-      if (day === 6) {
-        return !(hour >= 8 && hour < 12);
-      }
-
-      return true;
+      }, 50);
+      setTimeout(function () {
+        clearInterval(interval);
+      }, 15000);
     }
 
-    function isBadgeEligibleTime() {
-      var now = new Date();
-      var hour = now.getHours();
-      var day = now.getDay();
-
-      if (day >= 1 && day <= 5) {
-        return hour >= 8 && hour < 17;
+    function setCookie(name, value, days) {
+      var expires = "";
+      if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = "; expires=" + date.toUTCString();
       }
-
-      if (day === 6) {
-        return hour >= 8 && hour < 12;
-      }
-
-      return false;
+      document.cookie = name + "=" + (value || "") + expires + "; path=/";
     }
 
-    /* ---------------- INIT ---------------- */
+    function getCookie(name) {
+      var nameEQ = name + "=";
+      var ca = document.cookie.split(';');
+      for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+      }
+      return null;
+    }
+
+    function activateTest182() {
+      window['optimizelyEdge'] = window['optimizelyEdge'] || [];
+      window['optimizelyEdge'].push({
+        type: "page",
+        pageName: "21098500546_tt182__sp_cta_test"
+      });
+    }
+
     function init() {
-      document.body.classList.add(variation_name);
 
-      if (shouldShowBadge()) {
-        var targetEl = document.querySelector("#hero > .e-con-inner .e-child > div:nth-child(2) p");
+      // For the pages except the service pages
+      if (!window.location.href.includes('/service/')) {
+        setCookie('TT-182-SettingCookieForNotTargetedURLs', 'true');
+      } else if (window.location.href.includes('/service/')) {
 
-        if (targetEl && !document.querySelector(".availability-badge")) {
-          var badge = document.createElement("div");
-          badge.className = "availability-badge";
-          badge.innerHTML =
-            '<p><span class="dot"></span><b>Doctors available now</b> · average connection within 15 minutes</p>';
+        let otherPagesCookie = getCookie('TT-182-SettingCookieForNotTargetedURLs');
+        let targetedPagesCookie = getCookie('TT-182-TestCookieServicePages');
 
-          targetEl.insertAdjacentElement("afterend", badge);
+        if (otherPagesCookie) {
+          if (targetedPagesCookie) {
+            activateTest182();
+          }
+        } else {
+          activateTest182();
         }
-      } else {
-        if (debug) console.log("Badge hidden due to time condition");
       }
 
-     /* ---------------- CONVERT GOALS TRACKING ---------------- */
-
-      helper.live(".availability-badge", "click", function () {
-        window._conv_q = window._conv_q || [];
-        _conv_q.push(["triggerConversion", "100037484"]); 
-      });
-
-      // Book Telehealth
-      helper.live('#hero .elementor-widget-button a[href^="https://app.13sick.com.au/request-consult?isTelehealth=true"]', "click", function () {
-
-        window._conv_q = window._conv_q || [];
-
-        if (isBadgeEligibleTime()) {
-          // Eligible Time
-          _conv_q.push(["triggerConversion", "100037480"]);
-        } else {
-          // NOT Eligible Time
-          _conv_q.push(["triggerConversion", "100037481"]);
-        }
-
-      });
-
-
-      // Request Home Visit
-      helper.live('#hero .elementor-widget-button a[href^="https://app.13sick.com.au/request-consult/?isHomeVisit=true"]', "click", function () {
-
-        window._conv_q = window._conv_q || [];
-
-        if (isBadgeEligibleTime()) {
-          // Eligible Time
-          _conv_q.push(["triggerConversion", "100037482"]);
-        } else {
-          // NOT Eligible Time
-          _conv_q.push(["triggerConversion", "100037483"]);
-        }
-
-      });
     }
 
-    /* ---------------- RUN ---------------- */
-    helper.waitForElement(
-      "#hero > .e-con-inner .e-child > div:nth-child(2) p",
-      init,
-      50,
-      15000
-    );
-
+    /* Initialize variation */
+    waitForElement("body", init);
   } catch (e) {
-    console.log(e, "error in Test cre-t-2-Availability_Badge");
+    if (debug) console.log(e, "error in Test " + variation_name);
   }
 })();

f79f39303ca5fbf0498d9692b8fcbb5cd2b68f53
Hexdrift <150795432+SARTHAK-RAUTELA@users.noreply.github.com>
2026-04-10 13:18:54 +0530

    js css

--- local_testing/Local2/variation/vB.js
+++ local_testing/Local2/variation/vB.js
@@ -1,7 +1,7 @@
 (function () {
   try {
     var debug = 1;
-    var variation_name = "cre-t-2-Availability_Badge";
+    var variation_name = "cre-t-3";
 
     /* ---------------- HELPER ---------------- */
     var _$;
@@ -35,29 +35,6 @@
             clearInterval(interval);
           }, delayTimeout);
         },
-
-        live: function (selector, event, callback, context) {
-          function addEvent(el, type, handler) {
-            if (el.attachEvent) el.attachEvent("on" + type, handler);
-            else el.addEventListener(type, handler);
-          }
-
-          Element.prototype.matches =
-            Element.prototype.matches ||
-            Element.prototype.webkitMatchesSelector ||
-            Element.prototype.msMatchesSelector;
-
-          addEvent(context || document, event, function (e) {
-            var el = e.target;
-            while (el && el !== document) {
-              if (el.matches(selector)) {
-                callback.call(el, e);
-                break;
-              }
-              el = el.parentElement;
-            }
-          });
-        }
       };
 
       return function (selector) {
@@ -67,108 +44,21 @@
 
     var helper = _$();
 
-
-
-    /* ---------------- TIME LOGIC ---------------- */
-    function shouldShowBadge() {
-      var now = new Date();
-      var hour = now.getHours();
-      var day = now.getDay();
-
-      if (day >= 1 && day <= 5) {
-        return !(hour >= 8 && hour < 17);
-      }
-
-      if (day === 6) {
-        return !(hour >= 8 && hour < 12);
-      }
-
-      return true;
-    }
-
-    function isBadgeEligibleTime() {
-      var now = new Date();
-      var hour = now.getHours();
-      var day = now.getDay();
-
-      if (day >= 1 && day <= 5) {
-        return hour >= 8 && hour < 17;
-      }
-
-      if (day === 6) {
-        return hour >= 8 && hour < 12;
-      }
-
-      return false;
-    }
-
     /* ---------------- INIT ---------------- */
     function init() {
       document.body.classList.add(variation_name);
 
-      if (shouldShowBadge()) {
-        var targetEl = document.querySelector("#hero > .e-con-inner .e-child > div:nth-child(2) p");
-
-        if (targetEl && !document.querySelector(".availability-badge")) {
-          var badge = document.createElement("div");
-          badge.className = "availability-badge";
-          badge.innerHTML =
-            '<p><span class="dot"></span><b>Doctors available now</p>';
-
-          targetEl.insertAdjacentElement("afterend", badge);
-        }
-      } else {
-        if (debug) console.log("Badge hidden due to time condition");
-      }
-
-      /* ---------------- CONVERT GOALS TRACKING ---------------- */
-
-      helper.live(".availability-badge", "click", function () {
-        window._conv_q = window._conv_q || [];
-        _conv_q.push(["triggerConversion", "100037484"]);
-      });
-
-      // Book Telehealth
-      helper.live('#hero .elementor-widget-button a[href^="https://app.13sick.com.au/request-consult?isTelehealth=true"]', "click", function () {
-
-        window._conv_q = window._conv_q || [];
-
-        if (isBadgeEligibleTime()) {
-          // Eligible Time
-          _conv_q.push(["triggerConversion", "100037480"]);
-        } else {
-          // NOT Eligible Time
-          _conv_q.push(["triggerConversion", "100037481"]);
-        }
-
-      });
-
-
-      // Request Home Visit
-      helper.live('#hero .elementor-widget-button a[href^="https://app.13sick.com.au/request-consult/?isHomeVisit=true"]', "click", function () {
-
-        window._conv_q = window._conv_q || [];
-
-        if (isBadgeEligibleTime()) {
-          // Eligible Time
-          _conv_q.push(["triggerConversion", "100037482"]);
-        } else {
-          // NOT Eligible Time
-          _conv_q.push(["triggerConversion", "100037483"]);
-        }
-
-      });
     }
 
     /* ---------------- RUN ---------------- */
     helper.waitForElement(
-      "#hero > .e-con-inner .e-child > div:nth-child(2) p",
+      ".elementor-button-text",
       init,
       50,
       15000
     );
 
   } catch (e) {
-    console.log(e, "error in Test cre-t-2-Availability_Badge");
+    console.log(e, "error in Test cre-t-3");
   }
 })();
